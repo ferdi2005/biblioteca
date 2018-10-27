d4ae599ad24f30f7246da17d37a44a94ac69c469
Ferdinando Traversa <ferdi.traversa@gmail.com>
2018-10-27 17:29:42 +0200

    Modifiche e miglioramenti prestiti.

--- app/controllers/prestiti_controller.rb
+++ app/controllers/prestiti_controller.rb
@@ -1,16 +1,23 @@
 class PrestitiController < ApplicationController
   before_action :qualcuno?
+  before_action :admin?, only: [:consegna]
   def create
     unless params[:id]
       redirect_to libri_path
       flash[:warning] = "Non è possibile richiedere un prestito senza specificare quale libro si voglia chiedere in prestito."
     end
     @libro = Libro.find(params[:id].to_i)
-    if utente_corrente.id == @libro.utente.id
+    if @prestito = Prestito.find_by(utente: utente_corrente, libro: @libro)
+      redirect_to @prestito
+      flash[:danger] = "Non è possibile richiedere un prestito per un libro che si ha già letto. Questa è la pagina del prestito con il quale hai letto questo libro."
+    elsif Prestito.find_by(libro: @libro, stato: 0) || Prestito.find_by(libro: @libro, stato: 1)
+      redirect_to libri_path
+      flash[:danger] = "Ci dispiace ma questo libro è stato già richiesto in prestito da altri."
+    elsif utente_corrente.id == @libro.utente.id
       redirect_to libri_path
       flash[:danger] = "Ehi tu! Ma questo è il tuo libro! Non puoi richiedere un prestito per il tuo libro stesso, ce l'hai già a casa!"
-      else
-      @prestito = Prestito.new(utente: utente_corrente, libro: @libro, stato: 0)
+    else
+      @prestito = Prestito.new(utente: utente_corrente, libro: @libro, stato: 0, scadenza: DateTime.now + 15.days)
       if @prestito.save
         redirect_to @prestito
         flash[:success] = "Prestito richiesto con successo."
@@ -18,10 +25,13 @@ class PrestitiController < ApplicationController
         redirect_to @prestito
         flash[:danger] = "Errore! #{@prestito.errors.each {|errore| puts errore}}"
       end
-    end
   end
+end
+
 
   def consegna
+    prestito = Prestito.find(params[:id])
+    prestito.update_attributes(stato: 1, consegna: DateTime.now)
   end
 
   def restituzione
@@ -32,10 +42,18 @@ class PrestitiController < ApplicationController
     if utente_corrente.admin?
       @utenti = Utente.all
     end
-    @prestiti = utente_corrente.prestiti
+    @daconsegnare_pers = []
+    utente_corrente.libro.each do |libro|
+      a = Prestito.where(stato: 0, libro: libro)
+      a.each do |pres|
+        @daconsegnare_pers.push(pres)
+      end
+    end
+    @prestiti = utente_corrente.prestito
   end
 
   def show
     @prestito = Prestito.find(params[:id].to_i)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "libri#index"
 
   get 'registrazione', to: 'utenti#new'
+  #TODO: mettere nella navbar solo admin
   post 'registrazione', to: 'utenti#create'
 
   get 'richiedi', to: 'prestiti#create'
@@ -11,9 +12,14 @@ Rails.application.routes.draw do
   post 'login', to: 'sessioni#create'
 
   get 'approvare', to: 'libri#approvare'
+  #TODO: mettere in navbar solo admin
   get 'approva', to: 'libri#approva'
 
   resources :libri
   resources :prestiti, only: [:show, :index]
+
+  get 'consegna', to: 'prestiti#consegna'
+
+  #TODO: Metterlo nella navbar come sottovoce di prestiti
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
